ce10d98ff7e9f48dc15b5f8e0e3fc4c820ec5676
Nelson Favour <wworkifyy@gmail.com>
2024-05-04 17:27:29 +0100

    Nav Component added

---
+++ src/Pages/Home/Component/HomeHeroSection.tsx
@@ -0,0 +1,30 @@
+function HomeHeroSection() {
+  return (
+    <div className="hero-section">
+      <section>
+        <div className="text-white">
+          <h3 className="text-5xl tracking-widest max-w-textWidth leading-relaxed">
+            Caring for your home, made Convinent.
+          </h3>
+          <div className="flex gap-5 text-xl">
+            <div>
+              <a href="" className="text-primaryColor">
+                Hire A Technician
+              </a>
+            </div>
+
+            <div>
+              <a href="" className="text-white">Find Customers</a>
+            </div>
+          </div>
+        </div>
+
+        <div>
+            <input type="text" />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default HomeHeroSection;
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+
+import Nav from "../Home/Component/Nav"
+import HomeHeroSection from "./Component/HomeHeroSection";
+
+function Home() {
+  return (
+    <div>
+        <div>
+            <Nav/>
+        </div>
+
+        <div>
+          <HomeHeroSection/>
+        </div>
+    </div>
+  )
+}
+
+export default Home;
