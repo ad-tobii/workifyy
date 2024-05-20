8827827ac5568dcd8f13af958b48dc5157ebd1b9
Nelson <favournelson82@gmail.com>
2024-05-20 17:28:43 +0100

    Responsive landing page

--- src/Pages/Home/Component/MoreSection.tsx
+++ src/Pages/Home/Component/MoreSection.tsx
@@ -1,61 +1,52 @@
-import { FaStar } from "react-icons/fa6";
-import { FaDollarSign } from "react-icons/fa";
-import { FaCheck } from "react-icons/fa6";
 
-function MoreSection() {
+
+const features = [
+  {
+    name: "Hapiness Pledge",
+    description:
+      "If you're not satisfied, we'll work to make it right ",
+   
+  },
+  {
+    name: "Vetted Workifier",
+    description:
+      "Workers are always background checked before joining the platform",
+  
+  },
+  {
+    name: "Dedicated Support ",
+    description:
+      "Friendly service when you need us-every day of the work",
+   
+  },
+  
+];
+
+export default function Example() {
   return (
-    <div>
-      <main>
-        <section className="flex justify-center self-center content-center mx-auto mt-28 laptop:flex-col">
-          <div className="bg-[url('/assets/Section2.jpg')] bg-center bg-cover bg-no-repeat w-[40rem] h-[90vh] rounded-sm laptop:mx-auto miniTablet:w-[23rem]"></div>
-
-          <div className="bg-[#f7faf7] w-[40rem] h-[90vh] rounded-sm pl-10 pt-14 laptop:mx-auto miniTablet:w-[23rem]  miniTablet:h-[110vh]  miniTablet:pl-5">
-            <h1 className=" font-bold text-5xl text-black mxa-w-[50rem] pb-10 leading-relaxed miniTablet:text-3xl">
-              Why businesses turn to Workifyy
-            </h1>
-            <div className="flex gap-10 text-black  pb-8 ">
-              <div>
-                <FaStar />
+    <div className="mt-[10rem]">
+      <div className="mx-auto  px-3">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-4xl font-semibold leading-7 text-white">
+            Your Satisfaction,{" "}
+            <span className="text-[#32CD32]">Guaranteed</span>
+          </h2>
+        </div>
+        <div className="mx-auto mt-[3rem] max-w-4xl ">
+          <dl className="flex justify-center self-center gap-[5rem]">
+            {features.map((feature) => (
+              <div key={feature.name}>
+                <dt className="text-2xl font-semibold leading-7 text-[#32CD32]">
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-white">
+                  {feature.description}
+                </dd>
               </div>
-              <ul>
-                <h2 className="font-bold text-xl ">Proof of quality</h2>
-                <li className="font-light ">
-                  Check any pro’s work samples, client reviews, and identity
-                  verification.
-                </li>
-              </ul>
-            </div>
-
-            <div className="flex gap-10  text-black pb-8">
-              <div>
-                <FaDollarSign />
-              </div>
-              <ul>
-                <h2 className="font-bold text-xl">No cost until you hire</h2>
-                <li className="font-light text-base max-w-[30rem]">
-                  Interview potential fits for your job, negotiate rates, and
-                  only pay for work you approve.
-                </li>
-              </ul>
-            </div>
-
-            <div className="flex gap-10  text-black">
-              <div>
-                <FaCheck />
-              </div>
-              <ul>
-                <h2 className="font-bold text-xl">Safe and secure</h2>
-                <li className="font-light text-base max-w-[30rem] miniTablet:max-w-[18rem]">
-                  Focus on your work knowing we help protect your data and
-                  privacy. We’re here with 24/7 support if you need it.
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-      </main>
+            ))}
+          </dl>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default MoreSection;
