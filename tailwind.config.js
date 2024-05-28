04ee20e5796cca1a8021a0ffad2bdb9d9a9307eb
ad-tobi <simplytobi55@gmail.com>
2024-05-28 20:00:41 +0100

    sign up adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ export default {
         xsMobile: { max: "20em" }, // max: 320px
         miniMobile: { max: "25.75em" }, // min: 360px, max: 412px
         mobile: {max: "25.875em" }, // max: 414px
-        miniTablet: { max: "31.25em" }, // min: 425px, max: 500px
+        miniTablet: { min: "425px",max: "31.25em" }, // min: 425px, max: 500px
         tablet: { max: "37.5em" }, // max: 600px
         miniLaptop: { max: "42em" }, // max: 672px
         laptop: { max: "56.25em" }, // min: 601px, max: 900px
         desktop: { max: "75em" }, // min: 901px, max: 1200px
-    'sm':'320px',
+    
       },
       colors: {
         primaryColor: "#32CD32",
