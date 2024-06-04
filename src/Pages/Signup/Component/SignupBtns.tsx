78ee9e39ecab63b0993bc3bee563d8acab7cf179
ad-tobi <simplytobi55@gmail.com>
2024-06-04 10:54:07 +0100

    almost done

--- src/Pages/Signup/Component/SignupBtns.tsx
+++ src/Pages/Signup/Component/SignupBtns.tsx
@@ -8,15 +8,15 @@ function SignupBtns({ selectedRadio }: FootProps) {
 
   const handleButtonClick = () => {
     if (selectedRadio === "radio1") {
-      navigate("/ProfessionalSignup"); 
+      navigate("/ProfessionalSignup");
     } else if (selectedRadio === "radio2") {
-      navigate("/ClientSignup"); 
+      navigate("/ClientSignup");
     }
   };
 
   return (
     <>
-      <div className="mt-12 flex justify-center miniTablet:hidden laptop:mt-[13%]">
+      <div className="tabs:mt-[5%] mt-12 flex justify-center miniTablet:hidden">
         <button
           className={`rounded-3xl bg-zinc-300 px-4 py-2 font-bold text-white mobile:mt-4 mobile:w-[19rem] mobile:text-xl miniTablet:mt-4 miniTablet:w-96 miniTablet:text-2xl ${
             selectedRadio
