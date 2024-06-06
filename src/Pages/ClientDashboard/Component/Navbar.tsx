dd5c227f8b6ee01f28ec950450c594ce6905d9b8
Ibrahim <162850569+MarwezJunior@users.noreply.github.com>
2024-06-06 22:29:35 +0100

    Edit the workifyy logo

--- src/Pages/ClientDashboard/Component/Navbar.tsx
+++ src/Pages/ClientDashboard/Component/Navbar.tsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-white">
       <div className="flex items-center justify-between">
-        <div className="text-xl font-bold">Workifyy</div>
+        <div className="ml-12 font-logoFonts text-3xl text-[#32CD32]">
+          Workifyy
+        </div>
         <div className="flex items-center space-x-4">
           <a href="#bid" className="hover:underline">
             Bid
@@ -19,7 +21,7 @@ const Navbar = () => {
             Notifications
           </a>
           <a href="#profile" className="flex items-center hover:text-gray-400">
-            <FaUserAlt size={16} />
+            <FaUserAlt size={20} />
           </a>
         </div>
       </div>
