5bcffafb7a00d8ef5fe0ce08fc230b4f1172b601
ad-tobi <simplytobi55@gmail.com>
2024-06-24 14:05:42 +0100

    colour changes

--- src/Pages/ClientDashboard/Component/HeroSection.tsx
+++ src/Pages/ClientDashboard/Component/HeroSection.tsx
@@ -1,13 +1,14 @@
 const HeroSection = () => {
-  return (  <div className="relative   flex items-start justify-between p-8">
+  return (
+    <div className="relative flex   items-start justify-between  p-8">
       <div>
         <h1 className="text-3xl font-bold  text-white">
           Complete these steps to stand out and hire fast
         </h1>
         <div className="mt-8 flex space-x-8">
-          <div className="flex flex-1 h-44 items-center rounded-md bg-[#1e1e23] p-8 text-white shadow-md">
+          <div className="flex h-44 flex-1 items-center rounded-md bg-[#1e1e23] p-8 text-white shadow-md">
             <div>
-              <p className="font-semibold text-xl">Required to hire</p>
+              <p className="text-xl font-semibold">Required to hire</p>
               <p className="mt-2 text-xl font-semibold">
                 Add a billing method. You could start hiring 3x faster.
               </p>
@@ -16,8 +17,8 @@ const HeroSection = () => {
               <span className="inline-block text-3xl">💵</span>
             </div>
           </div>
-          <div className="flex text-xl flex-1 items-center rounded-md bg-[#1e1e23] p-8 text-white shadow-md">
-            <div className='-mt-6'>
+          <div className="flex flex-1 items-center rounded-md bg-[#1e1e23] p-8 text-xl text-white shadow-md">
+            <div className="-mt-6">
               <p className="font-semibold">Required to hire</p>
               <p className="mt-2 font-semibold">
                 You verified your email address.
@@ -29,9 +30,7 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      
     </div>
-  
   );
 };
 
--- src/Pages/ClientDashboard/Home.tsx
+++ src/Pages/ClientDashboard/Home.tsx
@@ -2,19 +2,17 @@ import Navbar from "./Component/Navbar";
 import HeroSection from "./Component/HeroSection";
 import JobButton from "./Component/JobButton";
 
+
 const ClientDashboard = () => {
   return (
-    <div className="bg-[#151519]  min-h-screen ">
+    <div className="h-screen  w-full bg-[#151519] ">
+      
       <Navbar />
-      <main className="w-max  p-8">
-        <HeroSection />
+      <main className="  p-8">
+        <HeroSection/>
         <div className="p-8">
           <JobButton />
         </div>
-
-
-
-
       </main>
     </div>
   );
