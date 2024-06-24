9ffaf7fb99ec379dd4b3c15965a61407c9c709ba
ad-tobi <simplytobi55@gmail.com>
2024-06-24 14:46:17 +0100

    colour changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,32 @@ import Signup from "./Pages/Signup/Signup";
 import { Signin } from "./Pages/Forms/signin/signin_user";
 import SignupasProfessional from "./Pages/Forms/professional/Professionalsignup";
 import ClientSignup from "./Pages/Forms/client/Clientsignup";
-import ClientDashboard from "./Pages/ClientDashboard/Home"; 
+import ClientDashboard from "./Pages/ClientDashboard/Home";
 import Jobs from "./Pages/ProfessionalDashboard/Jobs";
 import Wallet from "./Pages/ClientDashboard/Component/Wallet";
 
+const NotFound = () => (
+  <div>
+    <h2>Page Not Found</h2>
+    <a href="/">Back to Home</a>
+  </div>
+);
 
 const App = () => {
   return (
     <Router>
       {/* <Navbar /> */}
       <Routes>
-        <Route path="" element={<Home />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route
-          path="/ProfessionalSignup"
-          element={<SignupasProfessional />}
-        ></Route>
-        <Route path="/ClientSignup" element={<ClientSignup />}></Route>
-        <Route
-          path="/Jobs"
-          element={<Jobs />}
-        ></Route>
-        <Route path="/clientDashboard" element={<ClientDashboard />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/professional-signup" element={<SignupasProfessional />} />
+        <Route path="/client-signup" element={<ClientSignup />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/client-dashboard" element={<ClientDashboard />} />
         <Route path="/wallet" element={<Wallet />} />
-        
-        
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      
     </Router>
   );
 };
